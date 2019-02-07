8ad95d06618001c76546c317ed108a1fe76ec43d
Louis Jensen <lbj7@duke.edu>
2019-02-07 17:18:11 -0500

    add random configuration files

--- src/view/XMLParser.java
+++ src/view/XMLParser.java
@@ -55,7 +55,7 @@ public class XMLParser {
             }
         }
         catch (Exception e) {
-            e.printStackTrace();
+           //Should Never happen - no response
         }
     }
 
