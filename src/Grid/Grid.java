3ab4b2241fba05b377025c2585c9611a69532a9d
Louis Jensen <lbj7@duke.edu>
2019-02-08 16:39:53 -0500

    uncomment predator prey

--- src/Grid/Grid.java
+++ src/Grid/Grid.java
@@ -53,7 +53,7 @@ public abstract class Grid {
             return new Segregation(map, myCurrentState, myNextState);
         }
         if (sim.equals("PredatorPrey")) {
-            //return new PredatorPrey(map);
+            return new PredatorPrey(map);
         }
         return null;
     }
@@ -67,6 +67,10 @@ public abstract class Grid {
         }
         else if (myXML.getSimulationType().equals("Segregation")){
             return new SegregationCell(shape);
+        } else if (myXML.getSimulationType().equals("Percolation")){
+            return new PercolationCell(shape);
+        } else if (myXML.getSimulationType().equals("PredatorPrey")){
+            return new PredatorPreyCell(shape);
         }
         return new GameOfLifeCell(shape);
     }
