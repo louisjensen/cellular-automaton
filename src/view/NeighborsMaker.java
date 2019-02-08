e161fe813e01de650073389c90dffb8eb9289b3c
Justin Kim <jbk41@duke.edu>
2019-02-08 15:18:03 -0500

    fixed pointy

--- src/view/NeighborsMaker.java
+++ src/view/NeighborsMaker.java
@@ -82,6 +82,6 @@ public class NeighborsMaker {
     final ArrayList<Point> triangleSidePointy = new ArrayList<Point>(){{
         add(new Point(-1, 0));
         add(new Point(1, 0));
-        add(new Point(0, -1));
+        add(new Point(0, 1));
     }};
 }
