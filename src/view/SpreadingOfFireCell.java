d1904fa53db882f0f8ff649a764274e2b9c8d6d0
Louis Jensen <lbj7@duke.edu>
2019-02-08 11:05:20 -0500

    update

---
+++ src/view/SpreadingOfFireCell.java
@@ -0,0 +1,22 @@
+package view;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javafx.scene.shape.Polygon;
+import java.util.Map;
+
+public class SpreadingOfFireCell extends Cell{
+
+    private int myRow;
+    private int myCol;
+    private int myState;
+    private String myShapeType;
+    private Polygon myShape;
+
+    public SpreadingOfFireCell(Polygon shape){
+        super(shape);
+
+    }
+
+}
